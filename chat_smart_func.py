b3c0f4d322f852ab98bda00e19fad9580fec76b7
eistop42 <eistopacademy@gmail.com>
2025-02-22 12:12:08 +0500

    Create chat_smart_func.py

---
+++ chat_smart_func.py
@@ -0,0 +1,37 @@
+import requests
+
+catalog = 'b1gtphdg2vndncqf33o7'
+token = 'AQVNyLWbtYTmPzaqDniPH2bsBjA_teTrHeXxkGff'
+
+
+url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+
+def get_answer(question):
+
+    prompt = {
+        "modelUri": f'gpt://{catalog}/yandexgpt/latest',
+        "completionOptions": {
+            "stream": False,
+            "temperature": 0.6,
+            "maxTokens": 1000
+        },
+        "messages" : [
+            {
+                "role": "user",
+                "text": f"{question}"
+            }
+        ]
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Api-Key {token}"
+    }
+
+
+    ans = requests.post(url=url, headers=headers, json=prompt)
+    print(ans)
+
+    ans = ans.json()['result']['alternatives'][0]['message']['text']
+    return ans
+
